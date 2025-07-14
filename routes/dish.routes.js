c8ed87c858680d87567ccca7e9faf8b86066e044
BuiAnh12 <buianh120403@gmail.com>
2025-07-14 14:02:24 +0700

    FEAT: API for topping and toppingGroup

--- routes/dish.routes.js
+++ routes/dish.routes.js
@@ -14,18 +14,18 @@ const {
 const router = express.Router();
 
 router.get("/:dish_id", getDishById);
-// router.get("/store/:store_id", authMiddleware, roleAuthMiddleware(["admin", "employee"]), getDishesByStoreId);
+// router.get("/store/:store_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager" ]), getDishesByStoreId);
 router.get("/store/:store_id", getDishesByStoreId);
-// router.post("/store/:store_id", authMiddleware, roleAuthMiddleware(["admin", "employee"]), createDish); 
+// router.post("/store/:store_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), createDish); 
 router.post("/store/:store_id", createDish); 
 
 
 
-// router.put("/:dish_id", authMiddleware, roleAuthMiddleware(["admin", "employee"]), updateDish); 
+// router.put("/:dish_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), updateDish); 
 router.put("/:dish_id", updateDish); 
-// router.delete("/:dish_id", authMiddleware, roleAuthMiddleware(["admin", "employee"]), deleteDish);
+// router.delete("/:dish_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), deleteDish);
 router.delete("/:dish_id", deleteDish);
 
-// router.post("/:dish_id/status", authMiddleware, roleAuthMiddleware(["admin", "employee"]), changeStatus);
+// router.post("/:dish_id/status", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), changeStatus);
 router.post("/:dish_id/status", changeStatus);
 module.exports = router;
---
+++ routes/topping.routes.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const authMiddleware = require("../middlewares/authMiddleware");
+const roleAuthMiddleware = require("../middlewares/roleAuthMiddleware");
+const {
+    getStoreToppings,
+    getDishToppings,
+    getToppingById,
+    getToppingGroupById,
+    createToppingGroup,
+    createToppingInGroup,
+    updateToppingInGroup,
+    deleteToppingInGroup,
+    deleteToppingGroup,
+
+} = require("../controllers/topping.controller");
+
+
+const router = express.Router();
+// router.get("/store/:store_id",authMiddleware,roleAuthMiddleware(["owner", "employee", "manager"]), getStoreToppings);
+router.get("/store/:store_id", getStoreToppings);
+
+// router.get("/topping-group/:group_id",authMiddleware,roleAuthMiddleware(["owner", "employee", "manager"]), getToppingGroupById);
+router.get("/topping-group/:group_id", getToppingGroupById);
+
+// router.get("/dish/:dish_id", authMiddleware,roleAuthMiddleware(["owner", "employee", "manager"]), getDishToppings);
+router.get("/dish/:dish_id", getDishToppings);
+
+// router.get("/topping-group/:group_id",authMiddleware,roleAuthMiddleware(["owner", "employee", "manager"]), getToppingById);
+router.get("/:topping_id", getToppingById);
+
+// router.post("store/:store_id/topping-group", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), createToppingGroup);
+router.post("/store/:store_id/topping-group", createToppingGroup);
+
+// router.post("/topping-group/:group_id/topping", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), createToppingInGroup);
+router.post("/topping-group/:group_id/topping", createToppingInGroup);
+
+// router.put("topping-group/:group_id/topping/:topping_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), updateToppingInGroup);
+router.put("/topping-group/:group_id/topping/:topping_id", updateToppingInGroup);
+
+// router.delete("topping-group/:group_id/topping/:topping_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), deleteToppingInGroup);
+router.delete("/topping-group/:group_id/topping/:topping_id", deleteToppingInGroup);
+
+// router.delete("topping-group/:group_id", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), deleteToppingGroup);
+router.delete("/topping-group/:group_id", deleteToppingGroup);
+
+module.exports = router;
