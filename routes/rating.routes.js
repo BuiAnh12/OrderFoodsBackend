56bc1d0e38713a035b89ce7f65fb34a08a77a451
BuiAnh12 <buianh120403@gmail.com>
2025-07-29 09:14:29 +0700

    FEAT: reply Rating API

--- routes/rating.routes.js
+++ routes/rating.routes.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const authMiddleware = require("../middlewares/authMiddleware");
 const validateMongoDbId = require("../middlewares/validateMongoDBId");
+const roleAuthMiddleware = require("../middlewares/roleAuthMiddleware")
 const {
   getAllStoreRating,
   getDetailRating,
   addStoreRating,
   editStoreRating,
   deleteStoreRating,
+  getRatingsByStore,
+  replyToRating
 } = require("../controllers/rating.controller");
 
 const router = express.Router();
@@ -19,5 +22,10 @@ router.post("/add-rating", authMiddleware, addStoreRating);
 router.put("/edit-rating/:ratingId", authMiddleware, validateMongoDbId("ratingId"), editStoreRating);
 
 router.delete("/delete-rating/:ratingId", authMiddleware, validateMongoDbId("ratingId"), deleteStoreRating);
+
+// GET /api/v1/rating?replied=true|false|undefined&page=1&limit=10&sort=-createdAt
+router.get("/", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), getRatingsByStore);
+
+router.patch("/:id/reply", authMiddleware, roleAuthMiddleware(["owner", "employee", "manager"]), replyToRating);
 
 module.exports = router;
